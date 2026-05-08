2dcc6457d13cb995cf422ca00b21cec877574a58
Derinsola Oduwole <nired231@gmail.com>
2026-05-08 11:21:52 +0100

    loans page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Apply from "./components/Apply";
+import LoanDetails from "./components/LoanDetails";
 
 function App() {
   return (
@@ -31,7 +32,8 @@ function App() {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/apply-loan" element={<Apply/>}/>
+            <Route path="/apply-loan" element={<Apply />} />
+            <Route path="/loandetails" element={<LoanDetails />} />
           </Routes>
         </main>
         <Footer />
